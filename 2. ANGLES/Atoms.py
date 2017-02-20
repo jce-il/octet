85c1c02a95b3cf2d7a62d73fac9e30022a71c050
pabloli <pablo.a.liberman@gmail.com>
2017-02-20 12:04:46 +0200

    Update and rename angles.py to Atoms.py

--- 2. ANGLES/Atoms.py
+++ 2. ANGLES/Atoms.py
@@ -36,38 +36,6 @@ BondAngle[9][0][9] = ["tricapped trigonal prismatic",[]]
 
 
 
-import numpy
-
-class AnglesCalculator:
-    def Radian2Degree(angleInRad):
-        return angleInRad * 180 / numpy.pi
-    def Degree2Radian(angleInDeg):
-        return angleInDeg * numpy.pi / 180
-    def NormalizeDegree(angle):
-        return angle % 360
-    def NormalizeRadian(angle):
-        return AnglesCalculator.Degree2Radian(AnglesCalculator.NormalizeDegree(AnglesCalculator.Radian2Degree(angle)))
-    def BetweenAtoms(BoneElectronPair, LonePair,ElectronDomain):
-        if BoneElectronPair in  BondAngle.keys():
-            if LonePair in BondAngle[BoneElectronPair].keys():
-                if ElectronDomain in BondAngle[BoneElectronPair][LonePair]:
-                    return BondAngle[BoneElectronPair][LonePair][ElectronDomain]
-        return []
-    def BetweenSubSystems(SubSys1,SubSys2):
-        if (len(SubSys1) != len(SubSys2)):
-            return -1.0
-        P = []
-        Q = []
-        PQ = []
-        for d in range(len(SubSys1)):
-            P.append(SubSys1[d]**2)
-            Q.append(SubSys2[d]**2)
-            PQ.append(SubSys1[d]*SubSys2[d])
-        npP = numpy.sqrt(sum(P))
-        npQ = numpy.sqrt(sum(Q))
-        npPQ = sum(PQ)
-        return AnglesCalculator.Radian2Degree(numpy.arccos(npPQ / (npP * npQ)))
-    
         
 #%%   P = 4i + 0j + 7k and Q = -2i + j + 3k
 print(AnglesCalculator.BetweenSubSystems([4,0,7],[-2,1,3]))
