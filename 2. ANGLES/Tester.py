566f4170fbae9388013f8e0138e1103cca0c0f22
pabloli <pablo.a.liberman@gmail.com>
2017-02-20 16:44:00 +0200

    Update Tester.py

--- 2. ANGLES/Tester.py
+++ 2. ANGLES/Tester.py
@@ -1,5 +1,13 @@
         
+import Atoms
+import Astronomy
+import Subsystems
+import datetime
+
 #%%   P = 4i + 0j + 7k and Q = -2i + j + 3k
-print(AnglesCalculator.BetweenSubSystems([4,0,7],[-2,1,3]))
-print(AnglesCalculator.BetweenAtoms(2,2,4))
-print (GetSolarPosition(31.78734, 35.18145))
+print(Subsystems.BetweenSubSystems([4,0,7],[-2,1,3]))
+
+print(Atoms.BetweenAtoms(2,2,4))
+
+
+print (Astronomy.GetSolarPosition(31.78734, 35.18145,datetime.datetime.now()))
